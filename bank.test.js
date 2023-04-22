a6b8519e14eff879788dfa7ede8b3da6fbc3ab10
samuelbadru <samuelbadru@outlook.com>
2023-04-22 16:56:22 +0100

    beforeEach used to create account object

--- bank.test.js
+++ bank.test.js
@@ -1,13 +1,22 @@
 const Account = require('./bank')
 
 describe('Account', () => {
+  let account;
+
+  beforeEach(() => {
+    account = new Account();
+  });
+
   test('account exists', () => {
     expect(Account).toBeDefined();
   });
 
   test('default balance is 0', () => {
-    const account = new Account();
     expect(account.balance).toBe(0);
+  });
+
+  test('deposits increase the balance by that amount', () => {
+
   });
 
 
